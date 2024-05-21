bb960b0b922c8fbc2a7d23a8af26ae332d4c1c24
MadamSagas <141463441+MadamSagas@users.noreply.github.com>
2024-05-21 02:58:37 +0000

    Agregue el codigo necesario

--- src/app.py
+++ src/app.py
@@ -8,8 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Planets, People, Starships, FavoritePlanets
+#traer todas las tables creados
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -37,14 +37,192 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
+    # variable donde se guardan los users que tre el query all_users
+    # print(all_users[0].serialize())
+    # print('tipo de dato': type(all_users[0].serialize()))
+    all_users = User.query.all()
+    users_serialized = []
+    for user in all_users:
+        users_serialized.append(user.serialize())
+    print(users_serialized)
+    return jsonify({"data": users_serialized}), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user/<int:id>', methods=['GET'])
+def get_single_user(id):
+    single_user = User.query.get(id)
+    if single_user is None:
+        return jsonify({"msg": "El usuario con el ID : {} NO EXISTE".format(id)}), 400
+    return jsonify({"data": single_user.serialize()}), 200
+    # user1 = Person.query.get(person_id)
 
-    return jsonify(response_body), 200
+@app.route('/people', methods=['GET'])
+def get_people():
+    #people_query = Person.query.all()
+    all_people = People.query.all()
+    people_serialized = []
+    for peop in all_people:
+        people_serialized.append(peop.serialize())
+    print(people_serialized)
+    return jsonify({"data": people_serialized}), 200
 
+@app.route('/people/<int:id>', methods=['GET'])
+def get_single_people(id):
+    single_people = People.query.get(id)
+    if single_people is None:
+        return jsonify({"msg": "El usuario con el ID : {} NO EXISTE".format(id)})
+    return jsonify({"data": single_people.serialize()}), 200
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    all_planets = Planets.query.all()
+    planets_serialized = []
+    for planet in all_planets:
+        planets_serialized.append(planet.serialize())
+    print(planets_serialized)
+    return jsonify({"data": planets_serialized})
+
+@app.route('/planets/<int:id>', methods=['GET'])
+def get_single_planet(id):
+    single_planet = Planets.query.get(id)
+    if single_planet is None:
+        return jsonify({"msg": "El planeta con el ID: {} NO EXISTE".format(id)})
+    return jsonify({"data": single_planet.serialize()}), 200
+
+@app.route('/starships', methods=['GET'])
+def get_starships():
+    all_starships = Starships.query.all()
+    starships_serialized = []
+    for starship in all_starships:
+        starships_serialized.append(starship.serialize())
+    print(starships_serialized)
+    return jsonify({"data": starships_serialized})
+
+@app.route('/starships/<int:id>', methods=['GET'])
+def get_single_starship(id):
+    single_starship = Starships.query.get(id)
+    if single_starship is None:
+        return jsonify({"msg": "El Starship con el ID: {} NO EXISTE".format(id)})
+    return jsonify({"data": single_starship.serialize()}), 200
+
+
+@app.route("/planet", methods=['POST'])
+def new_planet():
+
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({"msg": "Debes enviar informacion en el body"}), 400
+    if "name" not in body:
+        return jsonify({"msg": "El campo name es obligatirio"}), 400
+    if "population" not in body:
+        return jsonify({"msg": "El campo population es obligatirio"}), 400
+    if "rotation_period" not in body:
+        return jsonify({"msg": "El campo rotation_period es obligatirio"}), 400
+    if "orbita_period" not in body:
+        return jsonify({"msg": "El campo orbita_period es obligarorio"}), 400
+    if "diameter" not in body:
+        return jsonify({"msg": "El campo diameter es obligatirio"}), 400
+    if "climate" not in body:
+        return jsonify({"msg": "El campo climate es obligatirio"}), 400
+    if "terrain"  not in body:
+        return jsonify({"msg": "El campo terrain es obligatirio"}), 400
+    new_planet = Planets()
+    new_planet.name = body["name"]
+    new_planet.population = body["population"]
+    new_planet.rotation_period = body["rotation_period"]
+    new_planet.orbita_period = body["orbita_period"]
+    new_planet.diameter = body["diameter"]
+    new_planet.climate = body["climate"]
+    new_planet.terrain = body["terrain"]
+    db.session.add(new_planet)
+    db.session.commit()
+    return jsonify({"msg": "Nuevo planeta creado",
+                    "data": new_planet.serialize()}), 201
+
+
+
+    '''
+    user1 = Person()
+    user1.username = "my_super_username"
+    user1.email = "my_super@email.com"
+    db.session.add(user1)
+    db.session.commit()
+    '''
+    
+@app.route("/people", methods=['POST'])
+def new_people():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({"msg": "Debes enviar informacion de el body"}), 400
+    if "name" not in body:
+        return jsonify({"msg": "El campo name es obligatirio"}), 400
+    if "height" not in body:
+        return jsonify({"msg": "El campo height es obligatorio"}), 400
+    if "mass" not in body:
+        return jsonify({"msg": "El campo mass es obligatorio"}), 400
+    if "birht_year" not in body:
+        return jsonify({"msg": "El campo birht_year es obligatorio"}), 400
+    if "gender" not in body:
+        return jsonify({"msg": "El campo gender es obligatorio"}), 400
+    new_people = People()
+    new_people.name = body["name"]
+    new_people.last_name = body["last_name"]
+    new_people.height = body["height"]
+    new_people.mass = body["mass"]
+    new_people.birht_year = body["birht_year"]
+    new_people.gender = body["gender"]
+    db.session.add(new_people)
+    db.session.commit()
+    return jsonify({"msg":"New People created",  
+                    "data": new_people.serialize()}), 201
+
+@app.route("/starships", methods=['POST'])
+def new_starship():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({"msg": "Debes enviar información en el body"}), 400
+    if "name" not in body:
+        return jsonify({"msg": "El campo name es obligatorio"}), 400
+    if "population" not in body:
+        return jsonify({"msg": "El campo population es obligatorio"}), 400
+    if "rotation_period" not in body:
+        return jsonify({"msg": "El campo rotation_period es obligatorio"}), 400
+    if "orbita_period" not in body:
+        return jsonify({"msg": "El campo orbita_period es obligatorio"}), 400
+    if  "diameter" not in body:
+        return jsonify({"msg":"El campo diameter es obligatorio"}), 400
+    if "climate" not in body:
+        return jsonify({"msg": "El campo climate es obligatorio"}), 400
+    if "terrain" not in body:
+        return jsonify({"msg": "El terrain climate es obligatorio" }), 400
+    new_starship = Starships()
+    new_starship.name = body["name"]
+    new_starship.population= body["population"]
+    new_starship.rotation_period = body["rotation_period"]
+    new_starship.orbita_period = body["orbita_period"]
+    new_starship.diameter = body["diameter"]
+    new_starship.climate = body["climate"]
+    new_starship.terrain = body["terrain"]
+    db.session.add(new_starship)
+    db.session.commit()
+    return jsonify({"msg": "New Starship created",
+                    "data": new_starship.serialize()}), 201
+
+@app.route('/user/<int:id>/favorites', methods=['GET'])
+def get_favorites(id):
+    user = User.query.get(id)
+    if user is None:
+        return jsonify({"msg": f"El usuariio con ID {id} existe"}), 404
+    favorite_planets = db.session.query(FavoritePlanets, Planets).join(Planets).\
+        filter(FavoritePlanets.user_id == id).all()
+    favorite_planets_serialized = []
+    for favorite_planet, planet in favorite_planets:
+        favorite_planets_serialized.append({'favorite_planet_id': favorite_planet.id, 
+                                            "planet": planet.serialize(), 
+                                            "user_id": id})
+    return jsonify({"msg": "OK", "data": favorite_planets_serialized})
+
+    
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -3,17 +3,116 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__= "user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return 'Usuario con email: {}'.format(self.email)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "is_active": self.is_active,
+             # do not serialize the password, its a security breach
+        }
+    
+class People(db.Model):
+    __tablename__ = "people"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50),)
+    height = db.Column(db.Integer, nullable=False)
+    mass = db.Column(db.Integer, nullable=False)
+    birht_year = db.Column(db.String(50), nullable=False)
+    gender = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"people {self.id} {self.name}"
+        
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
+            "height": self.height,
+            "mass": self.mass,
+            "birht_year": self.birht_year,
+            "gender": self.gender
+            }
+
+
+class Starships(db.Model):
+    __tablename__= "starships"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    model = db.Column(db.String(50))
+    manufacturer = db.Column(db.String(50))
+    length = db.Column(db.Integer)
+    crew = db.Column(db.Integer)
+    passengers = db.Column(db.Integer)
+    consumables = db.Column(db.String(50))
+
+    def __repr__(self):
+        return f"starship {self.id} {self.name}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "length": self.length,
+            "crew": self.crew,
+            "passengers": self.passengers,
+            "consumables": self.consumables
+
+        }
+
+
+class Planets(db.Model):
+    __tablename__= "planets"
+    id = db.Column(db.Integer, primary_key= True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    rotation_period = db.Column(db.Integer,nullable=False)
+    orbita_period = db.Column(db.Integer, nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
+    climate = db.Column(db.String(50), nullable=False)
+    terrain = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"planet {self.id} {self.name}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "rotation_period": self.rotation_period,
+            "orbita_period": self.orbita_period,
+            "diameter": self.diameter,
+            "climate": self.climate,
+            "terrain": self.terrain,
+        }
+
+class FavoritePlanets(db.Model):
+    __tablename__= "favorite_planets"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id',))
+    user_id_relationship = db.relationship(User)
+    planet_id_relationship = db.relationship(Planets)
+
+    def __repr__(self):
+        return f"Al usuario {self.user_id} le gusta el planeta {self.planet_id}"
+    
+    def rerialize(self):
+        return {
+            "id": self.id, 
+            "user_id": self.user_id,
+            "planet_id": self.planet_id
         }
